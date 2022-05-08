caf32c3511f431f15140ea4d06da6337599c6a41
root <root@localhost.localdomain>
2022-05-08 22:33:24 +0600

    Modified app.py file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World'
+    return 'Welcome to Page Cloud Acedamy'
 
 @app.errorhandler(500)
 def server_error(e):
